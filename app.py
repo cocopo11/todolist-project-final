9a87ef5584dfd059933e20c9c0f07cf6b86c23d3
cocopo11 <wntjsdid@gmail.com>
2024-07-10 16:09:38 +0900

    FEAT: 키워드 검색 기능 추가

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,7 +13,31 @@ tasks = []
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/search', methods=['GET'])
+def search():
+    keyword = request.args.get('keyword', '').strip()
+    if keyword:
+        filtered_tasks = [task for task in tasks if keyword in task.title]
+    else:
+        filtered_tasks = tasks
+
+    return render_template('index.html', tasks=filtered_tasks, keyword=keyword)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+'''
+<!-- index.html 파일에 아래 추가 -->
+<form action="/search" method="GET">
+    <input type="text" name="keyword" placeholder="검색어 입력">
+    <button type="submit">검색</button>
+</form>
+
+<ul>
+    {% for task in tasks %}
+    <li>{{ task.title }} - {{ task.difficulty }}</li>
+    {% endfor %}
+</ul>
+'''
